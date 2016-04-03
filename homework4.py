9db055f7704a3c37a9d332213bf52dff7c618bfa
Anton Bobrovsky <ileon009@gmail.com>
2016-04-03 21:12:10 +0600

    Доработал конечный автомат льва в homework4.py

--- homework4.py
+++ homework4.py
@@ -1,46 +1,49 @@
 class LionStates:
-    def __init__(self, state, symbol):
+    def __init__(self, state):
         if state == "сытый" or state == "голодный":
             self.state = state
         else:
             raise ValueError("Неверное состояние !")
 
-        if symbol == "антилопа" or symbol == "охотник" or symbol == "дерево":
-            self.symbol = symbol
-        else:
-            raise ValueError("Неверный входной символ !")
-
         self.action = ""
 
-    def implementation_fsm(self):
+    def implementation_fsm(self, symbol):
+        if symbol == "антилопа":
+            self.__antelope()
+        elif symbol == "охотник":
+            self.__hunter()
+        elif symbol == "дерево":
+            self.__tree()
+
+    def __antelope(self):
         if self.state == "сытый":
-            if self.symbol == "антилопа":
-                self.action = "спать"
-                self.state = "голодный"
-                print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
-
-            elif self.symbol == "охотник":
-                self.action = "убежать"
-                self.state = "голодный"
-                print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
-
-            elif self.symbol == "дерево":
-                self.action = "смотреть"
-                self.state = "голодный"
-                print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
+            self.action = "спать"
+            self.state = "голодный"
+            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
 
         elif self.state == "голодный":
-            if self.symbol == "антилопа":
-                self.action = "съесть"
-                self.state = "сытый"
-                print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
+            self.action = "съесть"
+            self.state = "сытый"
+            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
 
-            elif self.symbol == "охотник":
-                self.action = "убежать"
-                self.state = "голодный"
-                print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
+    def __hunter(self):
+        if self.state == "сытый":
+            self.action = "убежать"
+            self.state = "голодный"
+            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
 
-            elif self.symbol == "дерево":
-                self.action = "спать"
-                self.state = "голодный"
-                print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
+        elif self.state == "голодный":
+            self.action = "убежать"
+            self.state = "голодный"
+            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
+
+    def __tree(self):
+        if self.state == "сытый":
+            self.action = "смотреть"
+            self.state = "голодный"
+            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
+
+        elif self.state == "голодный":
+            self.action = "спать"
+            self.state = "голодный"
+            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
