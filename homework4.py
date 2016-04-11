228d3f8a960fc4f41a0382ec62f5ac24833c04f5
Anton Bobrovsky <ileon009@gmail.com>
2016-04-11 20:55:28 +0600

    Доработал льва

--- homework4.py
+++ homework4.py
@@ -1,10 +1,11 @@
 class LionStates:
     def __init__(self, state):
-        self.action = ""
-        if state.lower() == "сытый" or state.lower() == "голодный":
+        state.lower()
+        if state == "сытый" or state == "голодный":
             self.state = state
         else:
             raise ValueError("Неверное состояние !")
+        self.action = ""
 
     def implementation_fsm(self, symbol):
         if symbol == "антилопа":
@@ -13,33 +14,29 @@ class LionStates:
             self.__hunter()
         elif symbol == "дерево":
             self.__tree()
+        else:
+            raise ValueError("Неверный входной символ !")
 
     def __antelope(self):
         if self.state == "сытый":
             self.action = "спать"
             self.state = "голодный"
-            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
         elif self.state == "голодный":
             self.action = "съесть"
             self.state = "сытый"
-            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
 
     def __hunter(self):
         if self.state == "сытый":
             self.action = "убежать"
             self.state = "голодный"
-            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
         elif self.state == "голодный":
             self.action = "убежать"
             self.state = "голодный"
-            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
 
     def __tree(self):
         if self.state == "сытый":
             self.action = "смотреть"
             self.state = "голодный"
-            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
         elif self.state == "голодный":
             self.action = "спать"
             self.state = "голодный"
-            print("Действие: " + self.action.upper() + ", текущее состояние: " + self.state.upper())
